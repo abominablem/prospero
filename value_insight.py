2ba3dd483ff1a922ec60b31e6f8bd0f034fcd9c0
Marcus Hickman <88216015+abominablem@users.noreply.github.com>
2022-02-01 00:34:34 +0000

    Removed explicit trace logging

--- value_insight.py
+++ value_insight.py
@@ -4,36 +4,33 @@ Created on Tue Sep 14 22:09:37 2021
 
 @author: marcu
 """
-
-from mh_logging import Logging
-from sqlite_database import TableCon
-
-Logging
+import os
+import sys
+sys.path.append("D:\\Users\\Marcus\\Documents\\R Documents\\Coding\\Python\\Packages")
+from mh_logging import log_class
+from global_vars import LOG_LEVEL
+log_class = log_class(LOG_LEVEL)
+from sqlite_tablecon import TableCon
 
 class Insight(TableCon):
     """
     This class wraps functions that allow prediction of values from an input
     string. These predictions are based on database of past values entered.
     """
-    def __init__(self, type, debug = False, trace = None):
-        self.name = self.__class__.__name__
-
-        super(Insight, self).__init__(db = '.\sqlite_db\insight',
-                                      table = type,
-                                      debug = debug)
-
-        inf_trace = {"source": "function call",
-                     "parent": self.name +".__init__"}
-
-        self.logging = Logging(log = True)
-        self.logging.log_trace(self, "__init__",
-                               trace = {"source": "initialise class",
-                                        "parent": self.name})
+    @log_class
+    def __init__(self, type, debug = False):
         self.type = type
+        self._debug = debug
 
-    def get_insight(self, values, column, trace = None, **f_kwargs):
-        self.logging.log_trace(self, "get_insight", trace)
+        super(Insight, self).__init__(
+            db = os.path.abspath('.\sqlite_db\insight.db'),
+            table = type,
+            debug = debug,
+            check_same_thread = False
+            )
 
+    @log_class
+    def get_insight(self, values, column, **f_kwargs):
         del_list = []
         for k, v in values.items():
             if k.lower() == column or v == '' or v is None:
@@ -41,17 +38,13 @@ class Insight(TableCon):
 
         for k in del_list:
             del values[k]
-
-        filtered = self.filter(values, column, **f_kwargs)
+        try:
+            filtered = self.filter(values, column, **f_kwargs)
+        except:
+            filtered = {}
         for k, v in filtered.items():
             filtered[k] = [t for t in v if t != "" and not t is None]
         return filtered
 
-    def close(self, trace = None):
-        self.logging.log_trace(self, "close", trace)
-        super().close()
-
-
-
 insight = Insight("renames", debug = True)
-print(insight.filter({"Composer": "Schein, Johann Hermann"}, "album"))
+# print(insight.filter({"Composer": "Schein, Johann Hermann"}, "album"))
